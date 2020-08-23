a01422fda8cea8624394d50f13535c9f2097f341
YudjinSud <jeka.sudakov@mail.ru>
2020-08-23 16:19:12 +0300

    basic drawing

--- src/index.js
+++ src/index.js
@@ -1,22 +1,38 @@
 import './styles.css';
-import {hi} from "./GraphLib"
+import { hi } from "./GraphLib"
+
+var can = document.getElementById("c");
+var ctx = can.getContext('2d');
+
+can.width = 1000;
+can.height = 1000;
 
 function draw() {
-    var canvas = document.getElementById('c');
-    if (canvas.getContext) {
-      var ctx = canvas.getContext('2d');
 
-      ctx.fillStyle = 'rgb(200, 0, 0)';
-      ctx.fillRect(10, 10, 50, 50);
+}
 
-      ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-      ctx.fillRect(30, 30, 50, 50);
-     }
-  }
+function drawVertex(x, y) {
+    ctx.arc(x, y, 5, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+can.addEventListener('mousedown', event => {
+    let rect = can.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+    console.log("Coordinate x: " + x,
+        "Coordinate y: " + y);
+    drawVertex(x,y);
+
+})
+
+can.addEventListener('mousemove', e => {
+    let offsetX = e.offsetX;
+
+})
 
 function main() {
     draw();
-    console.log(hi());
 }
 
 main();
