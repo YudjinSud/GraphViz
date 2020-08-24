b902c9a974de4da3f2a99fef0191c2a4b02098fe
elek34ka <xtkcbfgkqwerty@gmail.com>
2020-08-24 10:19:59 +0300

    Updates

--- main.c9ce2e98575710edbb1a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{L1EO:function(t,n,e){},QfWi:function(t,n,e){"use strict";e.r(n);e("L1EO");!function(){var t=document.getElementById("c");if(t.getContext){var n=t.getContext("2d");n.fillStyle="rgb(200, 0, 0)",n.fillRect(10,10,50,50)}}(),console.log("hi")}},[["QfWi",1]]]);
-//# sourceMappingURL=main.c9ce2e98575710edbb1a.js.map
